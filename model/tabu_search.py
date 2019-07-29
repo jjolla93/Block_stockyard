f8a2a83ec999ae70a9d1bafa0f9aab7b7f81c209
jjolla93 <38776798+jjolla93@users.noreply.github.com>
2019-07-29 15:59:13 +0900

    휴리스틱 알고리즘 파일 추가

---
+++ model/tabu_search.py
@@ -0,0 +1,22 @@
+from simulater import DataManager
+import operator
+
+
+class TabuSearch(object):
+    def __init__(self, path, width=5, height=5):
+        # 입력 데이터 가져오기 및 멤버변수 저장
+        space, blocks = DataManager.import_data_from_csv(path, width, height)
+        self.NUM_BLOCKS = len(blocks)
+        self.BLOCKS = sorted(blocks, key=operator.attrgetter('_startdate'))
+        self.height = space.height
+        self.width = space.width
+
+    def arrange_blocks(self):
+        positions = []  # 결정된 각 블록의 배치 지번을 리스트로 출력
+        return positions
+
+
+if __name__ == "__main__":
+    tabu = TabuSearch('../data/test_data3.csv')
+    arranged = tabu.arrange_blocks()
+    print(arranged)
