1f3c1157dfe1c1d2519512dc815ab1744b93a953
jjolla93 <38776798+jjolla93@users.noreply.github.com>
2019-07-29 16:04:58 +0900

    설명 추가

--- model/heuristic_abslap.py
+++ model/heuristic_abslap.py
@@ -6,13 +6,14 @@ class HeuristicABSLAP(object):
     def __init__(self, path, width=5, height=5):
         # 입력 데이터 가져오기 및 멤버변수 저장
         space, blocks = DataManager.import_data_from_csv(path, width, height)
-        self.NUM_BLOCKS = len(blocks)
-        self.BLOCKS = sorted(blocks, key=operator.attrgetter('_startdate'))
+        self.num_block = len(blocks)
+        self.blocks = sorted(blocks, key=operator.attrgetter('_startdate'))
         self.height = space.height
         self.width = space.width
 
     def arrange_blocks(self):
-        positions = [] #결정된 각 블록의 배치 지번을 리스트로 출력
+        yard = np.full([self.height, self.width], 0) #적치장의 각 지번 0으로 초기화 함. 여기에 값 채워서 계산
+        positions = []  # 결정된 각 블록의 배치 지번을 리스트로 출력
         return positions
 
 if __name__ == "__main__":
--- model/tabu_search.py
+++ model/tabu_search.py
@@ -1,17 +1,20 @@
 from simulater import DataManager
 import operator
+import numpy as np
 
 
 class TabuSearch(object):
     def __init__(self, path, width=5, height=5):
         # 입력 데이터 가져오기 및 멤버변수 저장
         space, blocks = DataManager.import_data_from_csv(path, width, height)
-        self.NUM_BLOCKS = len(blocks)
-        self.BLOCKS = sorted(blocks, key=operator.attrgetter('_startdate'))
+        self.num_block = len(blocks)
+        #block 클래스에 각 블록의 입고일과 출고일이 datetime으로 들어 있음
+        self.blocks = sorted(blocks, key=operator.attrgetter('_startdate'))
         self.height = space.height
         self.width = space.width
 
     def arrange_blocks(self):
+        yard = np.full([self.height, self.width], 0) #적치장의 각 지번 0으로 초기화 함. 여기에 값 채워서 계산
         positions = []  # 결정된 각 블록의 배치 지번을 리스트로 출력
         return positions
 
